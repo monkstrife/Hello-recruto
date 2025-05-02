8ba8d45318ca1b4d6d40b87a74c5194d4df804ab
monkstrife <drobilko2002@mail.ru>
2025-05-02 21:44:24 +0300

    Добавление модели для валидации входных значений с использованием pydantic, typing для предоставления пустых значений, для того, чтобы использовать валидацию в get-эндпоинте, использую Depends(), BaseModel работает с данным JSON, поэтому нужен Depends(), чтобы оно брало значения из запроса "..?name=..." и мапила их в BaseModel

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+# Модель для валидации входных параметров
+class GreetingRequest(BaseModel):
+    name: Optional[str] = "Recruto"
+    message: Optional[str] = "Давай дружить"
 
 # GET-эндпоинт для обработки запроса
 @app.get("/greet")
-async def greet(name: str = None, message: str = None):
-    # Устанавливаем значения по умолчанию, если параметры не переданы
-    name = name if name else "Recruto"
-    message = message if message else "Давай дружить"
-    
+async def greet(req: GreetingRequest = Depends()): 
     # Формируем ответ
-    response = f"Hello {name}! {message}!"
+    response = f"Hello {req.name}! {req.message}!"
     return {"message": response}
